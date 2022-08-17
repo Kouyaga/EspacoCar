c40edbfbb2283b83e8768f6c4c5632f976d1c520
Leonardo <53689632+Kouyaga@users.noreply.github.com>
2022-08-17 15:55:30 -0300

    17/08 - Using Ajax.

--- wwwroot/js/Auth.js
+++ wwwroot/js/Auth.js
@@ -1,44 +1,49 @@
-﻿function verificarLogin() {
-    usuarioAuth = "admin";
-    login = $('#login').val();
-    
-    if (login == usuarioAuth) {
-        $("#senha").show();
-        $("#login").hide();
-        $("#botaoLogin").hide('slow');
-        $("#botaoCancelar").show('slow');
-        $("#botaoSenha").show('slow');
-        $("#fundoVerSenha").hide();
-        $("#fundoEsconderSenha").show();
-    } else {
-        Swal.fire({
-            title: 'Usuário Inválido!',
-            backdrop: false,
-            toast: true,
-            position: 'top',
-            text: '',
-            icon: 'error',
-            iconColor: 'darkred',
-            cancelButtonColor: 'darkred',
-            confirmButtonColor: 'darkred',
-            confirmButtonText: 'Fechar',
-            background: 'gray',
-            timer: 5000,
-            timerProgressBar: true,
-            customClass: {
-                popup: 'font-family'
+﻿function VerificarLogin() {
+    login = $("#login").val();
+    $.ajax({
+        type: "POST",
+        url: document.location.origin + "/Home/VerificarLogin",
+        data: { login },
+        success: function (retorno) {
+            if (retorno.ok) {
+                $("#senha").show();
+                $("#login").hide();
+                $("#botaoLogin").hide('slow');
+                $("#botaoCancelar").show('slow');
+                $("#botaoSenha").show('slow');
+                $("#fundoVerSenha").hide();
+                $("#fundoEsconderSenha").show();
+            } else {
+                Swal.fire({
+                    title: 'Usuário Inválido!',
+                    backdrop: false,
+                    toast: true,
+                    position: 'top',
+                    text: '',
+                    icon: 'error',
+                    iconColor: 'darkred',
+                    cancelButtonColor: 'darkred',
+                    confirmButtonColor: 'darkred',
+                    confirmButtonText: 'Fechar',
+                    background: 'gray',
+                    timer: 5000,
+                    timerProgressBar: true,
+                    customClass: {
+                        popup: 'font-family'
+                    }
+                })
+                $("#senha").hide();
+                $("#login").show();
+                $("#botaoLogin").show('slow');
+                $("#botaoCancelar").hide('slow');
+                $("#botaoSenha").hide('slow');
+                $("#fundoVerSenha").hide();
+                $("#fundoEsconderSenha").show();
+
             }
-        })
-        $("#senha").hide();
-        $("#login").show();
-        $("#botaoLogin").show('slow');
-        $("#botaoCancelar").hide('slow');
-        $("#botaoSenha").hide('slow');
-        $("#fundoVerSenha").hide();
-        $("#fundoEsconderSenha").show();
+        },
+    })    
     }
-   
-}
 
 function verificarSenha() {
     senhaAuth = "admin";
